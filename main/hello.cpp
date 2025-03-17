369c991ea0acd64a6a05ccca8633979100379561
Sakhi Saswat Panda <113613706+PandaTGOS@users.noreply.github.com>
2025-03-17 23:21:27 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(void) {
   cout << "Hello, World\n";
-  cout << "Hello, Jenkins\n";
+  cout << "Hello, Jenkins Change_2\n";
   cout << "I, PES2UG22CS489 have successfully built and run this  \n";
   return 0;
 }
